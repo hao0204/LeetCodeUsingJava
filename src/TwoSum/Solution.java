f6cff670867c1a254851d27d95ffa465f4fc276a
Think <332422832@qq.com>
2016-07-12 23:20:01 +0800

    add the answer of No.1 questions.

---
+++ src/TwoSum/Solution.java
@@ -0,0 +1,42 @@
+package TwoSum;
+
+import java.util.HashMap;
+
+public class Solution {
+	
+//	public static int[] twoSum(int[] nums, int target) {
+//        int[] answer = new int[2];
+//        A:for (int i = 0; i < nums.length; ++i){
+//        	answer[0] = i;
+//        	int b = target - nums[i];
+//        	for (int j = i + 1; j < nums.length; ++j){
+//        		if (nums[j] == b){
+//        			answer[1] = j;
+//        			break A;
+//        		}
+//        	}
+//        }
+//        return answer;
+//    }
+	
+	public static int[] twoSum(int[] nums, int target) {
+      int[] answer = new int[2];
+      HashMap<Integer, Integer> map = new HashMap<>();
+      for (int i = 0; i < nums.length; ++i){
+    	  map.put(nums[i], i);
+      }
+      for (int i = 0; i < nums.length; ++i){
+    	  int b = target - nums[i];
+      	if (map.containsKey(b) && i != map.get(b))
+      		return new int[]{i, map.get(b)};
+      }
+      return answer;
+  }
+	
+	public static void main(String[] args) {
+		int[] nums = {2, 7, 11, 15};
+		int[] d = twoSum(nums, 9);
+		System.out.println(d[0] + " " + d[1]);
+	}
+
+}
