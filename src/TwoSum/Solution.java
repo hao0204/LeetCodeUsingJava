db0961a3b3b94bc9ada3a4511dab165cccb67616
Think <332422832@qq.com>
2016-07-13 10:57:59 +0800

    adjust the space of code

--- src/TwoSum/Solution.java
+++ src/TwoSum/Solution.java
@@ -32,16 +32,16 @@ public class Solution {
 	 * @return
 	 */
 	public int[] twoSum(int[] nums, int target) {
-      int[] answer = new int[2];
-      HashMap<Integer, Integer> map = new HashMap<>();
-      for (int i = 0; i < nums.length; ++i){
-    	  map.put(nums[i], i);
-      }
-      for (int i = 0; i < nums.length; ++i){
-    	  int b = target - nums[i];
-      	if (map.containsKey(b) && i != map.get(b))
-      		return new int[]{i, map.get(b)};
-      }
-      return answer;
+		int[] answer = new int[2];
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; ++i){
+			map.put(nums[i], i);
+		}
+		for (int i = 0; i < nums.length; ++i){
+			int b = target - nums[i];
+			if (map.containsKey(b) && i != map.get(b))
+				return new int[]{i, map.get(b)};
+		}
+		return answer;
 	}
 }
