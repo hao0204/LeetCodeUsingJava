751ccf74e085ec6fdbfc01b7c75d01f4e7ec432a
Think <332422832@qq.com>
2016-10-18 14:14:58 +0800

    add no.258

---
+++ src/addDigits/Solution.java
@@ -0,0 +1,10 @@
+package addDigits;
+
+public class Solution {
+	public int addDigits(int num) {
+		if (num == 0) return num;
+        int a = num % 9;
+        if (a == 0) return 9;
+        else return a;
+    }
+}
