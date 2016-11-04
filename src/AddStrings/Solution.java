4cada9a48719eb4f07201e48b8d8eb0318aed151
Think <332422832@qq.com>
2016-11-04 23:00:58 +0800

    add no.326

---
+++ src/AddStrings/Solution.java
@@ -0,0 +1,16 @@
+package AddStrings;
+
+/**
+ * Created by Think on 11/4/16.
+ */
+public class Solution {
+//    public String addStrings(String num1, String num2) {
+//        int len1 = num1.length();
+//        int len2 = num2.length();
+//        if (len1 < len2) return addStrings(num2, num1);
+//        char[] n1 = num1.toCharArray();
+//        char[] n2 = num2.toCharArray();
+//        for (int i = len1 - 1, j = len2 - 1, k = 0; j >= 0; ++k){
+//        }
+//    }
+}
---
+++ src/PowerofThree/Solution.java
@@ -0,0 +1,10 @@
+package PowerofThree;
+
+/**
+ * Created by Think on 11/4/16.
+ */
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        return  n > 0 && 1162261467 % n == 0;
+    }
+}
