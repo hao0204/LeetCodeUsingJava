1f6da4e9ff8c23d39e9de0a12298b6da6e02cdda
Think <332422832@qq.com>
2016-12-26 15:52:55 +0800

    add no.204

---
+++ src/CountPrimes/Test.java
@@ -0,0 +1,11 @@
+package CountPrimes;
+
+/**
+ * Created by Think on 12/26/16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countPrimes(10));
+    }
+}
