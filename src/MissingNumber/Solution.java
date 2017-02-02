b3831888bed4d1d50a58488189f6b444e1c6bbc2
Think <332422832@qq.com>
2017-02-02 15:01:28 +0800

    add no.268

---
+++ src/MissingNumber/Solution.java
@@ -0,0 +1,15 @@
+package MissingNumber;
+
+/**
+ * Created by Think on 2/2/17.
+ */
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+
+        return xor ^ i;
+    }
+}
---
+++ src/MissingNumber/Test.java
@@ -0,0 +1,11 @@
+package MissingNumber;
+
+/**
+ * Created by Think on 2/2/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.missingNumber(new int[]{2,3}));
+    }
+}
