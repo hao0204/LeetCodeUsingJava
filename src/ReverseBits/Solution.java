e7f43f85bda0e2e8fd943a9975cafe76e29c26a8
Think <332422832@qq.com>
2017-02-07 23:20:34 +0800

    add no.190

--- src/ReverseBits/Solution.java
+++ src/ReverseBits/Solution.java
@@ -7,10 +7,9 @@ public class Solution {
     public int reverseBits(int n) {
         int result = 0;
         for (int i = 0; i < 32; ++i) {
-            result = n & 1;
+            int a = n & 1;
             n = n >>> 1;
-            if (i < 31)
-                result = result << 1;
+            result = (result << 1) + a; // or result = result << 1 | a;
         }
         return result;
     }
---
+++ src/ReverseBits/Test.java
@@ -0,0 +1,11 @@
+package ReverseBits;
+
+/**
+ * Created by Think on 2/7/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseBits(43261596));
+    }
+}
