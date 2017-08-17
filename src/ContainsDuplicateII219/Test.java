13c74a75e17ebcd9cf84fb05d2c1fdf18e98425f
Think <332422832@qq.com>
2017-08-17 15:19:27 +0800

    add no.219.

---
+++ src/ContainsDuplicateII219/Test.java
@@ -0,0 +1,11 @@
+package ContainsDuplicateII219;
+
+/**
+ * Created by Think on 8/16/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.containsNearbyDuplicate(new int[]{1,2,1}, 1));
+    }
+}
